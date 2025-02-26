ed03ce5afcf825efe0600afdf7028bbc459bf78c
Yixuan Liang <yixuanliang@YixuandeMacBook-Pro.local>
2025-02-25 21:58:02 -0800

    Add GitHub Actions CI workflow and configure JaCoCo

--- order-api/src/main/java/com/yixuanliang/userapi/UserApiApplication.java
+++ order-api/src/main/java/com/yixuanliang/userapi/UserApiApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OrderApiApplication {
+public class UserApiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(OrderApiApplication.class, args);
+        SpringApplication.run(UserApiApplication.class, args);
     }
 }
